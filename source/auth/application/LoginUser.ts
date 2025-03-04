8b84123ccd2d26264ea297ca85c9eb7e1336ae59
Carlos Núñez <carlitosnu41@gmail.com>
2025-03-04 16:19:44 -0600

    ✨ [Enchancement](Login) - Desactivar cuenta tras cinco días

--- source/auth/application/LoginUser.ts
+++ source/auth/application/LoginUser.ts
@@ -5,6 +5,7 @@ import type IUserRepository from "../../user/domain/IUserRepository";
 import UserInvalidPasswordError from "../../user/domain/UserInvalidPasswordError";
 import UserNotFoundError from "../../user/domain/UserNotFoundError";
 import type PasswordHasher from "../../user/infrastructure/PasswordHasher";
+import UserValidationError from "../../user/validators/UserValidationError";
 
 export default class LoginUserService implements IService<string, [string, string]> {
   constructor(
@@ -14,11 +15,21 @@ export default class LoginUserService implements IService<string, [string, strin
     private responseWrapper: typeof ResponseWrapper
   ) { };
 
+  private isAvailable(date: Date): boolean {
+    const now = Date.now();
+    const dif = now - date.getDate();
+    const fiveDays = 1000 * 60 * 60 * 24 * 5;
+
+    return dif < fiveDays;
+  }
   public async service(args: [string, string]): Promise<IServiceResponse<string>> {
     try {
       const [email, password] = args;
       const user = await this.repository.getByEmail(email);
       const isPasswordValid = this.hasher.testPassword(password, user.password);
+      if (!user.verified)
+        if (!this.isAvailable(user.createdAt))
+          throw new UserValidationError("El usuario no se ha verificado! La cuenta se ha desactivado.");
       if (!isPasswordValid)
         throw new UserInvalidPasswordError();
       const token = await this.jwtSerivce.encode(user);
